38078d794fa92c52d3ce30136710f79d8f591924
Lisandro <127849494+Lenut210@users.noreply.github.com>
2023-03-21 10:38:31 -0300

    Add files via upload

---
+++ LisandroEJ8.py
@@ -0,0 +1,17 @@
+print ("EJERCICIO N8      CALCULAR PERIMETRO ")
+print ("-------------------------------------------------")
+
+#Entrada
+print ("Ingrese base y altura")
+
+Base = Float(input("Base:"))
+Altura = Float(input("Altura:"))
+
+SUP= Base*Altura
+PER=2*(Base*Altura)
+#Salida
+print("----------------------------")
+print ("Superficie: ",SUP)
+print ("Perimetro: ",PER)
+
+
