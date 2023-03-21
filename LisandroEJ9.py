6a28533260a3efd3e1ffc13f2a7c8c87f1dc6a7d
Lisandro <127849494+Lenut210@users.noreply.github.com>
2023-03-21 11:33:16 -0300

    Add files via upload

---
+++ LisandroEJ9.py
@@ -0,0 +1,22 @@
+print ("EJERCICIO N9     Gasolinera ")
+print ("-------------------------------------------------")
+
+
+litxG=3.785
+precioXL=4.50
+#Entrada
+
+consu=float(input("Ingresar consumo:"))
+
+#Proceso
+Total = consu*litxG*precioXL 
+
+
+#Salida
+print("----------------------------------------")
+print("Total:",Total)
+
+
+
+
+
