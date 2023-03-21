551556ab60746e91d7e08a8481da221ca6609ab3
Lisandro <127849494+Lenut210@users.noreply.github.com>
2023-03-21 10:27:12 -0300

    Add files via upload

---
+++ LisandroEJ7.py
@@ -0,0 +1,17 @@
+print ("EJERCICIO N7 promedio de 2 numeros ")
+print ("-------------------------------------------------")
+
+#Entradas
+print ("Ingrese la nota 1")
+N1 = int(input(N1 :))
+print ("Ingrese la nota 2")
+N2 = int (input(N2:))
+
+#proceso
+
+S = N1 + N2 
+P = S/2
+
+#Salida
+
+print ("El promedio es:",P)
